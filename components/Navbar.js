251be9ae9b26b931f1f1fc6d691706301d629dda
Ashish1965 <121724473+Ashish1965@users.noreply.github.com>
2024-02-13 00:33:43 +0530

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -24,7 +24,7 @@ const Navbar = () => {
     const cookie = nookies.get();
     const token = cookie.token;
   return (
-    <nav className="bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
+    <nav className="fixed w-full shadow-xl z-[100] bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
           <img
